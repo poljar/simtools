968bf06f5f472938da98385ea606f12340577103
Damir Jelić <poljar@termina.org.uk>
2024-01-23 19:05:39 +0100

    Initial support to set the background color of a button

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 use std::{ffi::CStr, iter, time::Duration};
 
-use anyhow::Result;
+use anyhow::{Context as _, Result};
 use cairo::{Format, ImageSurface};
 use clap::{Parser, Subcommand};
 use embedded_graphics::{
@@ -15,6 +15,7 @@ use embedded_graphics::{
     text::{Alignment, Text},
     Drawable, Pixel,
 };
+use hidapi::{HidApi, HidDevice};
 use itertools::Itertools;
 use rusb::{
     request_type, Context, DeviceHandle, Direction, Error, Recipient, RequestType, UsbContext,
@@ -31,6 +32,7 @@ enum CliCommand {
     Draw,
     ShowDeviceDetails,
     GetConfigValue,
+    SetButtonColor { red: u8, green: u8, blue: u8 },
     SetBrightness { brightness: u8 },
 }
 
@@ -507,9 +509,51 @@ fn draw_letter(display: &mut USBD480Display) -> Result<()> {
     Ok(())
 }
 
+struct LmxButtons {
+    inner: HidDevice,
+}
+
+impl LmxButtons {
+    const VID: u16 = 0x5758;
+    const PID: u16 = 0xffff;
+
+    pub fn open(hidapi: &HidApi) -> Result<Self> {
+        let inner = hidapi
+            .open(Self::VID, Self::PID)
+            .context("Could not open the LM-X Wheel")?;
+
+        Ok(Self { inner })
+    }
+
+    #[inline(always)]
+    fn calculate_checksum(bytes: &[u8]) -> u8 {
+        let sum: u32 = bytes.iter().map(|&byte| byte as u32).sum();
+        (sum % 256) as u8
+    }
+
+    pub fn set_color(&self, r: u8, g: u8, b: u8) -> Result<()> {
+        // Repord ID ?    ?     ?  LED Nr. ?     Br   B  G  R   ?     ?   Checksum
+        let mut package = [
+            0x00, 0xff, 0xaa, 0x43, 0x01, 0x02, 0xe1, b, g, r, 0x00, 0x00, 0x00,
+        ];
+
+        let checksum = Self::calculate_checksum(&package[..package.len() - 1]);
+        package[package.len() - 1] = checksum;
+
+        self.inner.write(&package).context(
+            "Couldn't send the HID package to set the RGB color of a button to the device",
+        )?;
+
+        Ok(())
+    }
+}
+
 fn main() -> Result<()> {
     let mut context = Context::new()?;
-    let mut display = USBD480Display::open(&mut context)?;
+    let hidapi = HidApi::new().context("Could not create a HidApi object")?;
+
+    let mut display = USBD480Display::open(&context)?;
+    let lmx = LmxButtons::open(&hidapi)?;
 
     let cli = Cli::parse();
 
@@ -527,7 +571,29 @@ fn main() -> Result<()> {
         CliCommand::GetConfigValue => {
             display.get_config_value()?;
         }
+        CliCommand::SetButtonColor { red, green, blue } => {
+            lmx.set_color(red, green, blue)?;
+        }
     }
 
     Ok(())
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn checksum() {
+        const EXAMPLE_PAYLOAD: &[u8] = b"\xff\xaaC\x01\x02\xe3\xff\x05\xff\x00\x01\xd6";
+
+        let checksum =
+            LmxButtons::calculate_checksum(&EXAMPLE_PAYLOAD[..EXAMPLE_PAYLOAD.len() - 1]);
+
+        assert_eq!(
+            checksum,
+            EXAMPLE_PAYLOAD[EXAMPLE_PAYLOAD.len() - 1],
+            "The calculated checksum should match the one from the example"
+        );
+    }
 }
